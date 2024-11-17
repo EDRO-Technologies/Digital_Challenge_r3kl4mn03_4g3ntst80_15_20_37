008ea81fa1dd45a56f276a5feab1d160f01d2b12
holy-jesus <hj@holy-coder.ru>
2024-11-17 11:02:56 +0500

    Улучшил синтаксис

--- testik/proxy.go
+++ testik/proxy.go
@@ -7,8 +7,11 @@ import (
 )
 
 func GetCache(dnsCache *Cache, request *dns.Msg, config Config) *dns.Msg {
+	if !config.UseCache {
+		return nil
+	}
 	question := request.Question[0]
-	if config.UseCache && (question.Qtype == dns.TypeA || question.Qtype == dns.TypeAAAA) {
+	if question.Qtype == dns.TypeA || question.Qtype == dns.TypeAAAA {
 		cached, found := dnsCache.Get(question.Name)
 		if found {
 			rr := cached.(*dns.RR)
@@ -21,7 +24,10 @@ func GetCache(dnsCache *Cache, request *dns.Msg, config Config) *dns.Msg {
 }
 
 func SetCache(dnsCache *Cache, response *dns.Msg, question dns.Question, config Config) {
-	if len(response.Answer) > 0 && config.UseCache {
+	if !config.UseCache {
+		return
+	}
+	if len(response.Answer) > 0 {
 		switch response.Answer[len(response.Answer)-1].(type) {
 		case *dns.A, *dns.AAAA:
 			dnsCache.Set(
@@ -34,22 +40,22 @@ func SetCache(dnsCache *Cache, response *dns.Msg, question dns.Question, config 
 	}
 }
 
-func ProcessRequest(dnsClient *dns.Client, dnsCache *Cache, request *dns.Msg, config Config) (*dns.Msg, error) {
+func ProcessRequest(proxy *Proxy, request *dns.Msg, config Config) (*dns.Msg, error) {
 	switch request.Opcode {
 	case dns.OpcodeQuery:
 		if len(request.Question) > 0 {
 
-			response := GetCache(dnsCache, request, config)
+			response := GetCache(&proxy.cache, request, config)
 			if response != nil {
 				return response, nil
 			}
 
-			response, _, err := dnsClient.Exchange(request, config.DNSServer)
+			response, _, err := proxy.client.Exchange(request, config.DNSServer)
 			if err != nil {
 				return nil, err
 			}
 
-			SetCache(dnsCache, response, request.Question[0], config)
+			SetCache(&proxy.cache, response, request.Question[0], config)
 
 			return response, nil
 		}
